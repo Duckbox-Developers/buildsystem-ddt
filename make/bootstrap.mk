88d46e733c3302550c7722df65108db81c3996e5
max10 <max_10@gmx.de>
2016-03-26 00:50:56 +0100

    glibc: bump to version 2.14.1-59

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -58,7 +58,7 @@ endif
 BINUTILS_VER  = 2.24.51.0.3-76
 GCC_VER       = 4.8.4-139
 LIBGCC_VER    = 4.8.4-148
-GLIBC_VER     = 2.14.1-56
+GLIBC_VER     = 2.14.1-59
 
 crosstool-rpminstall: \
 $(ARCHIVE)/stlinux24-cross-sh4-binutils-$(BINUTILS_VER).i386.rpm \
