511f1d1c95cd42284a0888fcf15c14729770c889
max10 <max_10@gmx.de>
2016-09-29 20:21:23 +0200

    add busybox patches

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -16,6 +16,9 @@ $(D)/busybox: $(D)/bootstrap $(ARCHIVE)/busybox-$(BUSYBOX_VER).tar.bz2 $(PATCHES
 	rm -fr $(BUILD_TMP)/busybox-$(BUSYBOX_VER)
 	$(UNTAR)/busybox-$(BUSYBOX_VER).tar.bz2
 	set -e; cd $(BUILD_TMP)/busybox-$(BUSYBOX_VER); \
+		$(PATCH)/busybox-$(BUSYBOX_VER)-gzip.patch; \
+		$(PATCH)/busybox-$(BUSYBOX_VER)-ip.patch; \
+		$(PATCH)/busybox-$(BUSYBOX_VER)-ntpd.patch; \
 		$(PATCH)/busybox-$(BUSYBOX_VER)-nandwrite.patch; \
 		$(PATCH)/busybox-$(BUSYBOX_VER)-unicode.patch; \
 		$(PATCH)/busybox-$(BUSYBOX_VER)-extra.patch; \
