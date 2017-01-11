e533f9f2450871090093a517790e0ca119423658
max_10 <max_10@gmx.de>
2017-01-11 22:32:31 +0100

    busybox: bump version to 1.26.2 (stable)

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1,11 +1,10 @@
 #
 # busybox
 #
-BUSYBOX_VERSION = 1.26.1
+BUSYBOX_VERSION = 1.26.2
 BUSYBOX_PATCH   = busybox-$(BUSYBOX_VERSION)-nandwrite.patch
 BUSYBOX_PATCH  += busybox-$(BUSYBOX_VERSION)-unicode.patch
 BUSYBOX_PATCH  += busybox-$(BUSYBOX_VERSION)-extra.patch
-BUSYBOX_PATCH  += busybox-$(BUSYBOX_VERSION)-Fix-dependency-for-IFUPDOWN_UDHCPC_CMD_OPTIONS.patch
 
 $(ARCHIVE)/busybox-$(BUSYBOX_VERSION).tar.bz2:
 	$(WGET) http://busybox.net/downloads/busybox-$(BUSYBOX_VERSION).tar.bz2
