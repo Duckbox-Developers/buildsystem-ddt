edc622470eddd5dc4e041fb1b68940b66c44c9cc
max_10 <max_10@gmx.de>
2017-07-04 22:08:28 +0200

    busybox: bump to version 1.27.0

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1,12 +1,12 @@
 #
 # busybox
 #
-BUSYBOX_VERSION = 1.26.2
+BUSYBOX_VERSION = 1.27.0
 BUSYBOX_SOURCE = busybox-$(BUSYBOX_VERSION).tar.bz2
 BUSYBOX_PATCH  = busybox-$(BUSYBOX_VERSION)-nandwrite.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VERSION)-unicode.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VERSION)-extra.patch
-BUSYBOX_PATCH += busybox-$(BUSYBOX_VERSION)-wget_fix_for_brain-damaged_HTTP_servers.patch
+#BUSYBOX_PATCH += busybox-$(BUSYBOX_VERSION)-wget_fix_for_brain-damaged_HTTP_servers.patch
 
 $(ARCHIVE)/$(BUSYBOX_SOURCE):
 	$(WGET) http://busybox.net/downloads/$(BUSYBOX_SOURCE)
