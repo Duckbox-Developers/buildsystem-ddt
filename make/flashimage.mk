e7737029ad40c8bed688314efbe4303f056ebc1d
max_10 <max_10@gmx.de>
2017-08-06 14:33:09 +0200

    add flashimage.mk

---
+++ make/flashimage.mk
@@ -0,0 +1,30 @@
+#
+#
+#
+ifneq ($(SUDOPASSWD),)
+
+flashimage:
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), fortis_hdbox octagon1008 ufs910 ufs922 ipbox55 ipbox99 ipbox9900 cuberevo cuberevo_mini cuberevo_mini2 cuberevo_250hd cuberevo_2000hd cuberevo_3000hd))
+	cd $(BASE_DIR)/flash/nor_flash && echo "$(SUDOPASSWD) \n" | sudo -S ./make_flash.sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), spark spark7162))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), atevio7500))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs912))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs913))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufc960))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), tf7700))
+	cd $(BASE_DIR)/flash/$(BOXTYPE) && echo "$(SUDOPASSWD) \n" | sudo -S ./$(BOXTYPE).sh
+endif
+
+endif
+
