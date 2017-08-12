53bc46e6e560744c6a97eda4556dff92ec23822c
max_10 <max_10@gmx.de>
2017-08-12 09:56:48 +0200

    [make.sh] Follow the new order!

--- make.sh
+++ make.sh
@@ -13,7 +13,7 @@ fi
 ##############################################
 
 if [ "$1" == -h ] || [ "$1" == --help ]; then
-	echo "Parameter 1: target system (1-37)"
+	echo "Parameter 1: target system (1-36)"
 	echo "Parameter 2: kernel (1-2)"
 	echo "Parameter 3: optimization (1-4)"
 	echo "Parameter 4: player (1-2)"
@@ -22,17 +22,6 @@ if [ "$1" == -h ] || [ "$1" == --help ]; then
 	echo "Parameter 7: Image (Enigma=1/2 Neutrino=3/4 (1-4)"
 	exit
 fi
-
-##############################################
-
-echo "
-  _______                     _____              _     _         _
- |__   __|                   |  __ \            | |   | |       | |
-    | | ___  __ _ _ __ ___   | |  | |_   _  ____| | __| |_  __ _| | ___ ___
-    | |/ _ \/ _\` | '_ \` _ \  | |  | | | | |/  __| |/ /| __|/ _\` | |/ _ | __|
-    | |  __/ (_| | | | | | | | |__| | |_| |  (__|   < | |_| (_| | |  __|__ \\
-    |_|\___|\__,_|_| |_| |_| |_____/ \__,_|\____|_|\_\ \__|\__,_|_|\___|___/
-"
 
 ##############################################
 
@@ -52,92 +41,87 @@ do
 done
 echo " [OK]"
 echo
-echo
 
 ##############################################
 
 case $1 in
-	[1-9] | 1[0-9] | 2[0-9] | 3[0-9]) REPLY=$1;;
+	[1-9] | 1[0-9] | 2[0-9] | 3[0-6]) REPLY=$1;;
 	*)
 		echo "Target receivers:"
-		echo "    1)  build for PC"
-		echo "    2)  Kathrein UFS-910"
-		echo "    3)  Kathrein UFS-912"
-		echo "    4)  Kathrein UFS-913"
-		echo "    5)  Kathrein UFS-922"
-		echo "    6)  Kathrein UFC-960"
-		echo "    7)  Topfield TF77X0 HDPVR"
-		echo "    8)  IPBOX55"
-		echo "    9)  IPBOX99"
-		echo "   10)  IPBOX9900"
-		echo "   11)  Cuberevo [ IPBOX 9000 ]"
-		echo "   12)  Cuberevo mini [ IPBOX 900 ]"
-		echo "   13)  Cuberevo mini2 [ IPBOX 910 ]"
-		echo "   14)  Cuberevo 250 [ IPBOX 91 ]"
-		echo "   15)  Cuberevo 9500HD [ 7000HD ]"
-		echo "   16)  Cuberevo 2000HD"
-		echo "   17)  Cuberevo mini_fta [ 200HD ]"
-		echo "   18)  Xsarius Alpha [ Cuberevo 3000HD ]"
-		echo "   19)  Fortis HDbox [ Fortis FS9000/9200 ]"
-		echo "   20)  Octagon SF1008P [ Fortis HS9510 ]"
-		echo "   21)  Atevio AV7500 [ Fortis HS8200 ]"
-		echo "   22)  Atemio AM520"
-		echo "   23)  Atemio AM530"
-		echo "   24)  Fortis HS7110"
-		echo "   25)  Fortis HS7119"
-		echo "   26)  Fortis HS7420"
-		echo "   27)  Fortis HS7429"
-		echo "   28)  Fortis HS7810A"
-		echo "   29)  Fortis HS7819"
-		echo "   30)  Edision Argus VIP1 v1 [ single tuner + 2 CI + 2 USB ]"
-		echo "   31)  SpiderBox HL-101"
-		echo "   32)  SPARK"
-		echo "   33)  SPARK7162"
-		echo "   34)  B4Team ADB 5800S"
-		echo "   35)  Vitamin HD5000"
-		echo "   36)  SagemCom 88 series"
-		echo "   37)  Ferguson Ariva @Link 200"
-		read -p "Select target (1-37)? ";;
+		echo
+		echo "  Kathrein             Fortis"
+		echo "    1)  UFS-910          7)  FS9000 / FS9200 (formerly Fortis HDbox)"
+		echo "    2)  UFS-912          8)  HS9510          (formerly Octagon SF1008P)"
+		echo "    3)  UFS-913          9)  HS8200          (formerly Atevio AV7500)"
+		echo "    4)  UFS-922         10)  HS7110"
+		echo "    5)  UFC-960         11)  HS7119"
+		echo "                        12)  HS7420"
+		echo "  Topfield              13)  HS7429"
+		echo "    6)  TF77X0 HDPVR    14)  HS7810A"
+		echo "                        15)  HS7819"
+		echo
+		echo "  AB IPBox             Cuberevo"
+		echo "   16)  55HD            19)  id."
+		echo "   17)  99HD            20)  mini"
+		echo "   18)  9900HD          21)  mini2"
+		echo "   19)  9000HD          22)  250HD"
+		echo "   20)  900HD           23)  9500HD / 7000HD"
+		echo "   21)  910HD           24)  2000HD"
+		echo "   22)  91HD            25)  mini_fta / 200HD"
+		echo "                        26)  3000HD / Xsarius Alpha"
+		echo
+		echo "  Fulan                Atemio"
+		echo "   27)  Spark           29)  AM520"
+		echo "   28)  Spark7162       30)  AM530"
+		echo
+		echo "  Various"
+		echo "   31)  Edision Argus VIP1 v1 [ single tuner + 2 CI + 2 USB ]"
+		echo "   32)  SpiderBox HL-101"
+		echo "   33)  B4Team ADB 5800S"
+		echo "   34)  Vitamin HD5000"
+		echo "   35)  SagemCom 88 series"
+		echo "   36)  Ferguson Ariva @Link 200"
+		echo
+		read -p "Select target (1-36)? ";;
 esac
 
 case "$REPLY" in
-	 1) PLATFORM="pc";BOXTYPE="generic";;
-	 2) PLATFORM="sh4";BOXTYPE="ufs910";;
-	 3) PLATFORM="sh4";BOXTYPE="ufs912";;
-	 4) PLATFORM="sh4";BOXTYPE="ufs913";;
-	 5) PLATFORM="sh4";BOXTYPE="ufs922";;
-	 6) PLATFORM="sh4";BOXTYPE="ufc960";;
-	 7) PLATFORM="sh4";BOXTYPE="tf7700";;
-	 8) PLATFORM="sh4";BOXTYPE="ipbox55";;
-	 9) PLATFORM="sh4";BOXTYPE="ipbox99";;
-	10) PLATFORM="sh4";BOXTYPE="ipbox9900";;
-	11) PLATFORM="sh4";BOXTYPE="cuberevo";;
-	12) PLATFORM="sh4";BOXTYPE="cuberevo_mini";;
-	13) PLATFORM="sh4";BOXTYPE="cuberevo_mini2";;
-	14) PLATFORM="sh4";BOXTYPE="cuberevo_250hd";;
-	15) PLATFORM="sh4";BOXTYPE="cuberevo_9500hd";;
-	16) PLATFORM="sh4";BOXTYPE="cuberevo_2000hd";;
-	17) PLATFORM="sh4";BOXTYPE="cuberevo_mini_fta";;
-	18) PLATFORM="sh4";BOXTYPE="cuberevo_3000hd";;
-	19) PLATFORM="sh4";BOXTYPE="fortis_hdbox";;
-	20) PLATFORM="sh4";BOXTYPE="octagon1008";;
-	21) PLATFORM="sh4";BOXTYPE="atevio7500";;
-	22) PLATFORM="sh4";BOXTYPE="atemio520";;
-	23) PLATFORM="sh4";BOXTYPE="atemio530";;
-	24) PLATFORM="sh4";BOXTYPE="hs7110";;
-	25) PLATFORM="sh4";BOXTYPE="hs7119";;
-	26) PLATFORM="sh4";BOXTYPE="hs7420";;
-	27) PLATFORM="sh4";BOXTYPE="hs7429";;
-	28) PLATFORM="sh4";BOXTYPE="hs7810a";;
-	29) PLATFORM="sh4";BOXTYPE="hs7819";;
-	30) PLATFORM="sh4";BOXTYPE="hl101";;
+	 1) PLATFORM="sh4";BOXTYPE="ufs910";;
+	 2) PLATFORM="sh4";BOXTYPE="ufs912";;
+	 3) PLATFORM="sh4";BOXTYPE="ufs913";;
+	 4) PLATFORM="sh4";BOXTYPE="ufs922";;
+	 5) PLATFORM="sh4";BOXTYPE="ufc960";;
+	 6) PLATFORM="sh4";BOXTYPE="tf7700";;
+	 7) PLATFORM="sh4";BOXTYPE="fortis_hdbox";;
+	 8) PLATFORM="sh4";BOXTYPE="octagon1008";;
+	 9) PLATFORM="sh4";BOXTYPE="atevio7500";;
+	10) PLATFORM="sh4";BOXTYPE="hs7110";;
+	11) PLATFORM="sh4";BOXTYPE="hs7119";;
+	12) PLATFORM="sh4";BOXTYPE="hs7420";;
+	13) PLATFORM="sh4";BOXTYPE="hs7429";;
+	14) PLATFORM="sh4";BOXTYPE="hs7810a";;
+	15) PLATFORM="sh4";BOXTYPE="hs7819";;
+	16) PLATFORM="sh4";BOXTYPE="ipbox55";;
+	17) PLATFORM="sh4";BOXTYPE="ipbox99";;
+	18) PLATFORM="sh4";BOXTYPE="ipbox9900";;
+	19) PLATFORM="sh4";BOXTYPE="cuberevo";;
+	20) PLATFORM="sh4";BOXTYPE="cuberevo_mini";;
+	21) PLATFORM="sh4";BOXTYPE="cuberevo_mini2";;
+	22) PLATFORM="sh4";BOXTYPE="cuberevo_250hd";;
+	23) PLATFORM="sh4";BOXTYPE="cuberevo_9500hd";;
+	24) PLATFORM="sh4";BOXTYPE="cuberevo_2000hd";;
+	25) PLATFORM="sh4";BOXTYPE="cuberevo_mini_fta";;
+	26) PLATFORM="sh4";BOXTYPE="cuberevo_3000hd";;
+	27) PLATFORM="sh4";BOXTYPE="spark";;
+	28) PLATFORM="sh4";BOXTYPE="spark7162";;
+	29) PLATFORM="sh4";BOXTYPE="atemio520";;
+	30) PLATFORM="sh4";BOXTYPE="atemio530";;
 	31) PLATFORM="sh4";BOXTYPE="hl101";;
-	32) PLATFORM="sh4";BOXTYPE="spark";;
-	33) PLATFORM="sh4";BOXTYPE="spark7162";;
-	34) PLATFORM="sh4";BOXTYPE="adb_box";;
-	35) PLATFORM="sh4";BOXTYPE="vitamin_hd5000";;
-	36) PLATFORM="sh4";BOXTYPE="sagemcom88";;
-	37) PLATFORM="sh4";BOXTYPE="arivalink200";;
+	32) PLATFORM="sh4";BOXTYPE="hl101";;
+	33) PLATFORM="sh4";BOXTYPE="adb_box";;
+	34) PLATFORM="sh4";BOXTYPE="vitamin_hd5000";;
+	35) PLATFORM="sh4";BOXTYPE="sagemcom88";;
+	36) PLATFORM="sh4";BOXTYPE="arivalink200";;
 	 *) PLATFORM="sh4";BOXTYPE="atevio7500";;
 esac
 echo "PLATFORM=$PLATFORM" > config
@@ -186,8 +170,8 @@ echo "OPTIMIZATIONS=$OPTIMIZATIONS" >> config
 case $4 in
 	[1-2]) REPLY=$4;;
 	*)	echo -e "\nPlayer:"
-		echo "   1)  Player 191 test (stmfb-3.1_stm24_0104, for internal testing)"
-		echo "   2)  Player 191      (stmfb-3.1_stm24_0104, recommended)"
+		echo "   1)  Player 191 (stmfb-3.1_stm24_0104, for internal testing)"
+		echo "   2)  Player 191 (stmfb-3.1_stm24_0104, recommended)"
 		read -p "Select player (1-2)? ";;
 esac
 
@@ -208,8 +192,8 @@ case $5 in
 	*)	echo -e "\nMedia Framework:"
 		echo "   1) eplayer3"
 		echo "   2) gstreamer"
-		echo "   3) use built-in (required for Neutrino)"
-		echo "   4) gstreamer+eplayer3 (recommended for OpenPLi)"
+		echo "   3) use built-in       (required for Neutrino)"
+		echo "   4) gstreamer+eplayer3 (required for OpenPLi)"
 		read -p "Select media framework (1-4)? ";;
 esac
 
@@ -247,7 +231,7 @@ case $7 in
 	[1-4]) REPLY=$7;;
 	*)	echo -e "\nWhich Image do you want to build:"
 		echo "   1)  Enigma2"
-		echo "   2)  Enigma2 (includes WLAN drivers)"
+		echo "   2)  Enigma2  (includes WLAN drivers)"
 		echo "   3)  Neutrino"
 		echo "   4)  Neutrino (includes WLAN drivers)"
 		read -p "Select Image to build (1-4)? ";;
