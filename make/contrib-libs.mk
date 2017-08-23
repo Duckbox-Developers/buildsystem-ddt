537cc747ff0494ae512f964b28ef2a69e11a1040
max_10 <max_10@gmx.de>
2017-08-23 13:25:17 +0200

    Update build graphlcd

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1944,7 +1944,7 @@ GRAPHLCD_PATCH = graphlcd-base-touchcol.patch
 $(ARCHIVE)/$(GRAPHLCD_SOURCE):
 	get-git-archive.sh $(GRAPHLCD_URL) $(GRAPHLCD_VERSION) $(notdir $@) $(ARCHIVE)
 
-$(D)/graphlcd: $(D)/bootstrap $(D)/freetype $(D)/libusb $(ARCHIVE)/$(GRAPHLCD_SOURCE)
+$(D)/graphlcd: $(D)/bootstrap $(D)/libiconv $(D)/freetype $(D)/libusb $(ARCHIVE)/$(GRAPHLCD_SOURCE)
 	$(START_BUILD)
 	$(REMOVE)/graphlcd-$(GRAPHLCD_VERSION)
 	$(UNTAR)/$(GRAPHLCD_SOURCE)
