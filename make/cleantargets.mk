9f2725dc1e6f9b5e6b52df133dc98ae35925523d
max_10 <max_10@gmx.de>
2017-09-03 20:33:02 +0200

    update cleantargets.mk

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -2,21 +2,25 @@ depsclean:
 	( cd $(D) && find . ! -name "*\.*" -delete )
 
 clean: depsclean
-	-$(MAKE) kernel-clean
-	-$(MAKE) tools-clean
-	-$(MAKE) driver-clean
-	-rm -rf $(BASE_DIR)/tufsbox
-	-rm -rf $(D)/kernel
-	-rm -rf $(D)/kernel.do_compile
+	@printf "$(TERM_YELLOW)---> cleaning system build directories and files .. $(TERM_NORMAL)"
+	@-$(MAKE) kernel-clean
+	@-$(MAKE) tools-clean
+	@-$(MAKE) driver-clean
+	@-rm -rf $(BASE_DIR)/tufsbox
+	@-rm -rf $(D)/kernel
+	@-rm -rf $(D)/kernel.do_compile
+	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 distclean:
-	-$(MAKE) tools-clean
-	-$(MAKE) driver-clean
-	-rm -rf $(BASE_DIR)/tufsbox
-	-rm -rf $(BUILD_TMP)
-	-rm -rf $(SOURCE_DIR)
-	-rm -rf $(D)
-	test -d $(D) || mkdir $(D)
+	@printf "$(TERM_YELLOW)---> cleaning system build directories and files .. $(TERM_NORMAL)"
+	@-$(MAKE) tools-clean
+	@-$(MAKE) driver-clean
+	@-rm -rf $(BASE_DIR)/tufsbox
+	@-rm -rf $(BUILD_TMP)
+	@-rm -rf $(SOURCE_DIR)
+	@-rm -rf $(D)
+	@test -d $(D) || mkdir $(D)
+	@printf "$(TERM_YELLOW)done\n$(TERM_NORMAL)"
 
 %-clean:
 	( cd $(D) && find . -name $(subst -clean,,$@) -delete )
