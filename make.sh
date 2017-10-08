b7bb98a0c3a5b56736fe4eb6064f90413e469129
max_10 <max_10@gmx.de>
2017-10-08 13:30:55 +0200

    armbox: enable reschange for armboxes

--- make.sh
+++ make.sh
@@ -13,7 +13,7 @@ fi
 ##############################################
 
 if [ "$1" == -h ] || [ "$1" == --help ]; then
-	echo "Parameter 1: target system (1-36)"
+	echo "Parameter 1: target system (1-38)"
 	echo "Parameter 2: kernel (1-2)"
 	echo "Parameter 3: optimization (1-4)"
 	echo "Parameter 4: player (1-2)"
@@ -45,7 +45,7 @@ echo
 ##############################################
 
 case $1 in
-	[1-9] | 1[0-9] | 2[0-9] | 3[0-7]) REPLY=$1;;
+	[1-9] | 1[0-9] | 2[0-9] | 3[0-8]) REPLY=$1;;
 	*)
 		echo "Target receivers:"
 		echo
@@ -82,9 +82,10 @@ case $1 in
 		echo "   35)  SagemCom 88 series"
 		echo "   36)  Ferguson Ariva @Link 200"
 		echo
-		echo "   37)  armbox for internal testing"
+		echo "   37)  Mutant HD51"
+		echo "   38)  VU Solo 4k"
 		echo
-		read -p "Select target (1-37)? ";;
+		read -p "Select target (1-38)? ";;
 esac
 
 case "$REPLY" in
@@ -124,7 +125,8 @@ case "$REPLY" in
 	34) BOXARCH="sh4";BOXTYPE="vitamin_hd5000";;
 	35) BOXARCH="sh4";BOXTYPE="sagemcom88";;
 	36) BOXARCH="sh4";BOXTYPE="arivalink200";;
-	37) BOXARCH="arm";BOXTYPE="armbox";;
+	37) BOXARCH="arm";BOXTYPE="hd51";;
+	38) BOXARCH="arm";BOXTYPE="vusolo4k";;
 	 *) BOXARCH="sh4";BOXTYPE="atevio7500";;
 esac
 echo "BOXARCH=$BOXARCH" > config
