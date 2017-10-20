f11352c3fd383d2054aa5fcb87dd3f3c209f298d
max_10 <max_10@gmx.de>
2017-10-20 22:14:10 +0200

    update patch SOURCE_DIR

--- make/patches.mk
+++ make/patches.mk
@@ -1,16 +1,17 @@
 #
 # diff helper
 #
-enigma2-patch \
-neutrino-hd2-patch \
-neutrino-mp-next-patch \
-neutrino-mp-tangos-patch \
-neutrino-mp-cst-next-patch \
-neutrino-mp-cst-next-ni-patch \
-libstb-hal-next-patch \
-libstb-hal-cst-next-patch \
-libstb-hal-cst-next-tangos-patch:
-	cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ]
+#enigma2-patch
+#neutrino-hd2-patch
+#neutrino-mp-next-patch
+#neutrino-mp-tangos-patch
+#neutrino-mp-cst-next-patch
+#neutrino-mp-cst-next-ni-patch
+#libstb-hal-next-patch
+#libstb-hal-cst-next-patch
+#libstb-hal-cst-next-tangos-patch
+%-patch:
+	( cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ] )
 
 # keeping all patches together in one file
 # uncomment if needed
