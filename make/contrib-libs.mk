ccb2393e63cc66a2d7977fb18cf2b7c9fe27d9d4
max_10 <max_10@gmx.de>
2017-11-01 11:54:18 +0100

    ncurses: fix gcc6 compile

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -3,6 +3,7 @@
 #
 NCURSES_VER = 5.9
 NCURSES_SOURCE = ncurses-$(NCURSES_VER).tar.gz
+NCURSES_PATCH = ncurses-$(NCURSES_VER)-gcc-5.x-MKlib_gen.patch
 
 $(ARCHIVE)/$(NCURSES_SOURCE):
 	$(WGET) https://ftp.gnu.org/pub/gnu/ncurses/$(NCURSES_SOURCE)
@@ -12,6 +13,7 @@ $(D)/ncurses: $(D)/bootstrap $(ARCHIVE)/$(NCURSES_SOURCE)
 	$(REMOVE)/ncurses-$(NCURSES_VER)
 	$(UNTAR)/$(NCURSES_SOURCE)
 	set -e; cd $(BUILD_TMP)/ncurses-$(NCURSES_VER); \
+		$(call post_patch,$(NCURSES_PATCH)); \
 		$(CONFIGURE) \
 			--target=$(TARGET) \
 			--prefix=/usr \
