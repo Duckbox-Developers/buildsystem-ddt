a312ad9243e369e3ad2f6a0fbb81219ebfe63a17
max_10 <max_10@gmx.de>
2017-11-09 20:19:05 +0100

    fix

--- make/crosstool-sh4.mk
+++ make/crosstool-sh4.mk
@@ -94,7 +94,7 @@ HOST_U_BOOT_TOOLS_VER = 1.3.1_stm24-9
 host_u_boot_tools: \
 $(STL_ARCHIVE)/stlinux24-host-u-boot-tools-$(HOST_U_BOOT_TOOLS_VER).i386.rpm
 	$(START_BUILD)
-	unpack-rpm.sh $(BUILD_TMP) $(STM_RELOCATE)/host/bin $(HOST_DIR)/bin \
+	$(SCRIPTS_DIR)/unpack-rpm.sh $(BUILD_TMP) $(STM_RELOCATE)/host/bin $(HOST_DIR)/bin \
 		$^
 	@touch $(D)/$(notdir $@)
 	@echo -e "Build of $(TERM_GREEN_BOLD)$@$(PKG_VER) $(TERM_NORMAL)completed."; echo
