32fe0d41989bd454f4435b87dc06a8949bc39c34
max_10 <max_10@gmx.de>
2017-12-17 12:43:05 +0100

    update build ffmpeg

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1450,8 +1450,10 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hds-libroxml.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-aac.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-kodi.patch
 
-FFMPEG_EXTERN = 
-FFMPEG_CONF_OPTS = --disable-armv5te --disable-armv6 --disable-armv6t2 --disable-neon
+FFMPEG_DEPS =
+FFMPEG_CONF_OPTS  = --disable-neon
+FFMPEG_CONF_OPTS += --enable-demuxer=hds
+FFMPRG_EXTRA_CFLAGS =
 else
 FFMPEG_VER = 3.2.2
 FFMPEG_SOURCE = ffmpeg-$(FFMPEG_VER).tar.xz
@@ -1460,14 +1462,17 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-add-dash-demux.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-mpegts.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow-to-choose-rtmp-impl-at-runtime.patch
 
-FFMPEG_EXTERN = $(D)/libxml2 $(D)/librtmpdump
-FFMPEG_CONF_OPTS = --enable-librtmp
+FFMPEG_DEPS = $(D)/libxml2 $(D)/librtmpdump
+FFMPEG_CONF_OPTS  = --enable-librtmp
+FFMPEG_CONF_OPTS += --enable-demuxer=dash
+FFMPEG_CONF_OPTS += --cpu=cortex-a15
+FFMPRG_EXTRA_CFLAGS =  -mfpu=neon-vfpv4 -mfloat-abi=hard
 endif
 
 $(ARCHIVE)/$(FFMPEG_SOURCE):
 	$(WGET) http://www.ffmpeg.org/releases/$(FFMPEG_SOURCE)
 
-$(D)/ffmpeg: $(D)/bootstrap $(D)/openssl $(D)/bzip2 $(D)/libass $(D)/libroxml $(FFMPEG_EXTERN) $(ARCHIVE)/$(FFMPEG_SOURCE)
+$(D)/ffmpeg: $(D)/bootstrap $(D)/openssl $(D)/bzip2 $(D)/libass $(D)/libroxml $(FFMPEG_DEPS) $(ARCHIVE)/$(FFMPEG_SOURCE)
 	$(START_BUILD)
 	$(REMOVE)/ffmpeg-$(FFMPEG_VER)
 	$(UNTAR)/$(FFMPEG_SOURCE)
@@ -1504,6 +1509,9 @@ $(D)/ffmpeg: $(D)/bootstrap $(D)/openssl $(D)/bzip2 $(D)/libass $(D)/libroxml $(
 			--disable-mipsdspr2 \
 			--disable-mipsfpu \
 			--disable-fast-unaligned \
+			--disable-armv5te \
+			--disable-armv6 \
+			--disable-armv6t2 \
 			\
 			--disable-dxva2 \
 			--disable-vaapi \
@@ -1590,7 +1598,6 @@ $(D)/ffmpeg: $(D)/bootstrap $(D)/openssl $(D)/bzip2 $(D)/libass $(D)/libroxml $(
 			--enable-demuxer=dts \
 			--enable-demuxer=flac \
 			--enable-demuxer=flv \
-			--enable-demuxer=hds \
 			--enable-demuxer=hls \
 			--enable-demuxer=image2 \
 			--enable-demuxer=image2pipe \
@@ -1658,7 +1665,7 @@ $(D)/ffmpeg: $(D)/bootstrap $(D)/openssl $(D)/bzip2 $(D)/libass $(D)/libroxml $(
 			\
 			--enable-cross-compile \
 			--cross-prefix=$(TARGET)- \
-			--extra-cflags="$(TARGET_CFLAGS)" \
+			--extra-cflags="$(TARGET_CFLAGS) $(FFMPRG_EXTRA_CFLAGS)" \
 			--extra-ldflags="$(TARGET_LDFLAGS) -lrt" \
 			--target-os=linux \
 			--arch=$(BOXARCH) \
