5d674575d43115e3e323aefd347c00762205b163
max_10 <max_10@gmx.de>
2018-01-11 21:13:14 +0100

    dvbsnoop: fix build sh4

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1473,7 +1473,11 @@ DVBSNOOP_URL = https://github.com/Duckbox-Developers/dvbsnoop.git
 $(ARCHIVE)/$(DVBSNOOP_SOURCE):
 	$(SCRIPTS_DIR)/get-git-archive.sh $(DVBSNOOP_URL) $(DVBSNOOP_VER) $(notdir $@) $(ARCHIVE)
 
-$(D)/dvbsnoop: $(D)/bootstrap $(ARCHIVE)/$(DVBSNOOP_SOURCE)
+ifeq ($(BOXARCH), sh4)
+DVBSNOOP_CONF_OPTS = --with-dvbincludes=$(KERNEL_DIR)/include
+endif
+
+$(D)/dvbsnoop: $(D)/bootstrap $(D)/kernel $(ARCHIVE)/$(DVBSNOOP_SOURCE)
 	$(START_BUILD)
 	$(REMOVE)/dvbsnoop-$(DVBSNOOP_VER)
 	$(UNTAR)/$(DVBSNOOP_SOURCE)
@@ -1482,6 +1486,7 @@ $(D)/dvbsnoop: $(D)/bootstrap $(ARCHIVE)/$(DVBSNOOP_SOURCE)
 			--enable-silent-rules \
 			--prefix=/usr \
 			--mandir=/.remove \
+			$(DVBSNOOP_CONF_OPTS) \
 		; \
 		$(MAKE); \
 		$(MAKE) install DESTDIR=$(TARGET_DIR)
