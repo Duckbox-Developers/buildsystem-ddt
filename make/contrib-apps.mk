47bdf7431b98054dc2426131abaa111535e074c9
BPanther <bpanther_ts@hotmail.com>
2018-08-22 08:31:55 +0200

    update e2fsprogs 1.44.4

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -311,7 +311,7 @@ $(D)/portmap: $(D)/bootstrap $(D)/lsb $(ARCHIVE)/$(PORTMAP_SOURCE) $(ARCHIVE)/po
 #
 # e2fsprogs
 #
-E2FSPROGS_VER = 1.44.3
+E2FSPROGS_VER = 1.44.4
 E2FSPROGS_SOURCE = e2fsprogs-$(E2FSPROGS_VER).tar.gz
 E2FSPROGS_PATCH = e2fsprogs-$(E2FSPROGS_VER).patch
 
