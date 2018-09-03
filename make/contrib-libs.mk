76c2f24fabb8abd2dddb2c568c4e4b64ccd56d7b
BPanther <bpanther_ts@hotmail.com>
2018-09-03 04:14:18 +0200

    fix graphlcd

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1765,7 +1765,9 @@ GRAPHLCD_VER = 55d4bd8
 GRAPHLCD_SOURCE = graphlcd-git-$(GRAPHLCD_VER).tar.bz2
 GRAPHLCD_URL = git://projects.vdr-developer.org/graphlcd-base.git
 GRAPHLCD_PATCH = graphlcd-git-$(GRAPHLCD_VER).patch
+ifeq ($(BOXTYPE), vusolo4k)
 GRAPHLCD_PATCH += graphlcd-vusolo4k.patch
+endif
 
 $(ARCHIVE)/$(GRAPHLCD_SOURCE):
 	$(SCRIPTS_DIR)/get-git-archive.sh $(GRAPHLCD_URL) $(GRAPHLCD_VER) $(notdir $@) $(ARCHIVE)
