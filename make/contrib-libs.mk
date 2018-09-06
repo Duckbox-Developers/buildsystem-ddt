d427bceaa5f16267b4aeb75ba3d4566a50618e5d
BPanther <bpanther_ts@hotmail.com>
2018-09-06 23:39:20 +0200

    update libcurl 7.61.1

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1000,7 +1000,7 @@ $(D)/libconfig: $(D)/bootstrap $(ARCHIVE)/$(LIBCONFIG_SOURCE)
 #
 # libcurl
 #
-LIBCURL_VER = 7.61.0
+LIBCURL_VER = 7.61.1
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_PATCH = libcurl-$(LIBCURL_VER).patch
 
