789c6651ac6987837fd97e920c9fbd47e8a0443d
Frankenstone <dampf_acc@yahoo.com>
2019-01-03 17:07:40 +0100

    contrib-libs.mk: fix bzip2

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -628,7 +628,7 @@ BZIP2_SOURCE = bzip2-$(BZIP2_VER).tar.gz
 BZIP2_Patch = bzip2-$(BZIP2_VER).patch
 
 $(ARCHIVE)/$(BZIP2_SOURCE):
-	$(WGET) http://www.bzip.org/$(BZIP2_VER)/$(BZIP2_SOURCE)
+	$(WGET) https://sourceforge.net/projects/bzip2/files//$(BZIP2_SOURCE)
 
 $(D)/bzip2: $(D)/bootstrap $(ARCHIVE)/$(BZIP2_SOURCE)
 	$(START_BUILD)
