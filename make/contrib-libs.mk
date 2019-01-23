509f82eac9010d6f39ceb74925962ec77fe4d912
BPanther <bpanther_ts@hotmail.com>
2019-01-23 19:58:54 +0100

    update alsa-util/lib 1.1.8

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1978,7 +1978,7 @@ $(D)/libusb_compat: $(D)/bootstrap $(D)/libusb $(ARCHIVE)/$(LIBUSB_COMPAT_SOURCE
 #
 # alsa-lib
 #
-ALSA_LIB_VER = 1.1.7
+ALSA_LIB_VER = 1.1.8
 ALSA_LIB_SOURCE = alsa-lib-$(ALSA_LIB_VER).tar.bz2
 ALSA_LIB_PATCH  = alsa-lib-$(ALSA_LIB_VER).patch
 ALSA_LIB_PATCH += alsa-lib-$(ALSA_LIB_VER)-link_fix.patch
@@ -2018,7 +2018,7 @@ $(D)/alsa_lib: $(D)/bootstrap $(ARCHIVE)/$(ALSA_LIB_SOURCE)
 #
 # alsa-utils
 #
-ALSA_UTILS_VER = 1.1.7
+ALSA_UTILS_VER = 1.1.8
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VER).tar.bz2
 
 $(ARCHIVE)/$(ALSA_UTILS_SOURCE):
