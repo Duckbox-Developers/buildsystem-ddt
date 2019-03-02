c35dcb9141a8234a9a0a70cc6f328f46fd1337b6
BPanther <bpanther_ts@hotmail.com>
2019-03-02 15:38:46 +0100

    update optional openVMP 2.4.7

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1651,7 +1651,7 @@ $(D)/udpxy: $(D)/bootstrap $(ARCHIVE)/$(UDPXY_SOURCE)
 #
 # openvpn
 #
-OPENVPN_VER = 2.4.6
+OPENVPN_VER = 2.4.7
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VER).tar.xz
 
 $(ARCHIVE)/$(OPENVPN_SOURCE):
