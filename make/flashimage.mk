a057bf82be94313f41bbb56e84c9090be1e88faf
BPanther <bpanther_ts@hotmail.com>
2019-04-04 11:12:11 +0200

    fix build vuduo - not yet ready

--- make/flashimage.mk
+++ make/flashimage.mk
@@ -33,6 +33,9 @@ endif
 ifeq ($(BOXTYPE), $(filter $(BOXTYPE), vusolo4k))
 	$(MAKE) flash-image-vusolo4k-multi-disk flash-image-vusolo4k-multi-rootfs
 endif
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), vuduo))
+	$(MAKE) flash-image-vuduo
+endif
 	$(TUXBOX_CUSTOMIZE)
 
 ofgimage:
@@ -331,3 +334,27 @@ flash-image-vusolo4k-online:
 	tar -cvzf $(BASE_DIR)/$(BOXTYPE)_multi_usb_$(shell date '+%d.%m.%Y-%H.%M').tgz rootfs.tar.bz2 initrd_auto.bin kernel_auto.bin reboot.update imageversion
 	# cleanup
 	rm -rf $(VUSOLO4K_BUILD_TMP)
+
+VUDUO_PREFIX = vuplus/duo
+VUDUO_BUILD_TMP = $(BUILD_TMP)/image-build
+flash-image-vuduo:
+	# Create final USB-image
+	mkdir -p $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)
+	touch $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/reboot.update
+	cp $(RELEASE_DIR)/boot/kernel_cfe_auto.bin $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)
+	mkfs.ubifs -r $(RELEASE_DIR) -o $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/root_cfe_auto.ubi -m 2048 -e 126976 -c 4096 -F
+	echo '[ubifs]' > $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'mode=ubi' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'image=$(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/root_cfe_auto.ubi' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'vol_id=0' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'vol_type=dynamic' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'vol_name=rootfs' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo 'vol_flags=autoresize' >> $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	ubinize -o $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/root_cfe_auto.jffs2 -m 2048 -p 128KiB $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	rm -f $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/root_cfe_auto.ubi
+	rm -f $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/ubinize.cfg
+	echo $(BOXTYPE)_DDT_usb_$(shell date '+%d%m%Y-%H%M%S') > $(VUDUO_BUILD_TMP)/$(VUDUO_PREFIX)/imageversion
+	cd $(VUDUO_BUILD_TMP)/ && \
+	zip -r $(BASE_DIR)/$(BOXTYPE)_usb_$(shell date '+%d.%m.%Y-%H.%M').zip $(VUDUO_PREFIX)*
+	# cleanup
+	rm -rf $(VUDUO_BUILD_TMP)
