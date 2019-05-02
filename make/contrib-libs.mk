95e9b29cc75960ca7a3565b34dfd11bc76c4080a
BPanther <bpanther_ts@hotmail.com>
2019-05-02 10:38:02 +0200

    update lcd4linux

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1868,12 +1868,11 @@ $(D)/libdpf: $(D)/bootstrap $(D)/libusb_compat $(ARCHIVE)/$(LIBDPF_SOURCE)
 #
 # lcd4linux
 #
-LCD4LINUX_VER = 07ef2dd
+LCD4LINUX_VER = aa65ff1
 LCD4LINUX_SOURCE = lcd4linux-git-$(LCD4LINUX_VER).tar.bz2
 LCD4LINUX_URL = https://github.com/TangoCash/lcd4linux.git
-LCD4LINUX_PATCH = lcd4linux-widget.patch
+LCD4LINUX_PATCH = 
 ifeq ($(BOXTYPE), vusolo4k)
-LCD4LINUX_PATCH += lcd4linux-vusolo4k.patch
 LCD4LINUX_DRV = ,VUSOLO4K
 endif
 
