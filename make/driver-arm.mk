70fce4b0bf7565f3d225fa96ab7c46e2644cfe2e
BPanther <bpanther_ts@hotmail.com>
2019-05-20 15:17:22 +0200

    revert driver update vusolo4k

--- make/driver-arm.mk
+++ make/driver-arm.mk
@@ -41,7 +41,7 @@ endif
 
 ifeq ($(BOXTYPE), vusolo4k)
 DRIVER_VER = 3.14.28
-DRIVER_DATE = 20190424
+DRIVER_DATE = 20181204
 DRIVER_REV = r0
 DRIVER_SRC = vuplus-dvb-proxy-$(KERNEL_TYPE)-$(DRIVER_VER)-$(DRIVER_DATE).$(DRIVER_REV).tar.gz
 
@@ -141,7 +141,7 @@ $(D)/driver: $(ARCHIVE)/$(DRIVER_SRC) $(D)/bootstrap $(D)/kernel
 # platform util
 #
 UTIL_VER = 17.1
-UTIL_DATE = 20190424
+UTIL_DATE = 20181204
 UTIL_REV = r0
 UTIL_SRC = platform-util-$(KERNEL_TYPE)-$(UTIL_VER)-$(UTIL_DATE).$(UTIL_REV).tar.gz
 
@@ -159,7 +159,7 @@ $(D)/platform_util: $(D)/bootstrap $(ARCHIVE)/$(UTIL_SRC)
 # libgles
 #
 GLES_VER = 17.1
-GLES_DATE = 20190424
+GLES_DATE = 20181204
 GLES_REV = r0
 GLES_SRC = libgles-$(KERNEL_TYPE)-$(GLES_VER)-$(GLES_DATE).$(GLES_REV).tar.gz
 
