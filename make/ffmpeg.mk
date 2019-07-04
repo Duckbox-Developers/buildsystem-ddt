eeed4915c09f70f488ee7a8b2cab81a571135e04
Frankenstone <dampf_acc@yahoo.com>
2019-07-04 14:45:59 +0200

    ffmpeg 2.18.15 add ’remove_avpriv_request_sample.patch’

--- make/ffmpeg.mk
+++ make/ffmpeg.mk
@@ -386,6 +386,7 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hds-libroxml.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-aac.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-kodi.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-tls.patch
+FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-remove_avpriv_request_sample.patch
 
 FFMPEG_DEPS =
 FFMPEG_CONF_OPTS = 
