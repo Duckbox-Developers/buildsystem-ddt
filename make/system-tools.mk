f6785f7cfcbbe185324550e2e9550fda4a876e06
BPanther <bpanther_ts@hotmail.com>
2019-08-16 10:09:46 +0200

    optional busybox snapshot changed to git

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -14,9 +14,7 @@ BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-unicode.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-extra.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-extra2.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-flashcp-small-output.patch
-
-$(ARCHIVE)/$(BUSYBOX_SOURCE):
-	$(WGET) https://busybox.net/downloads/$(BUSYBOX_SOURCE)
+BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-block-telnet-internet.patch
 
 ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
 BUSYBOX_CONFIG = busybox-$(BUSYBOX_VER).config_arm
@@ -26,10 +24,25 @@ else
 BUSYBOX_CONFIG = busybox-$(BUSYBOX_VER).config
 endif
 
+ifeq ($(BUSYBOX_SNAPSHOT), 1)
+BUSYBOX_PATCH += busybox-snapshot-tar-fix.patch
+$(D)/busybox: $(D)/bootstrap $(PATCHES)/$(BUSYBOX_CONFIG)
+	$(START_BUILD)
+	$(REMOVE)/busybox$(BB_SNAPSHOT)
+	set -e; if [ -d $(ARCHIVE)/busybox.git ]; \
+		then cd $(ARCHIVE)/busybox.git; git pull; \
+		else cd $(ARCHIVE); git clone git://git.busybox.net/busybox.git busybox.git; \
+		fi
+	cp -ra $(ARCHIVE)/busybox.git $(BUILD_TMP)/busybox$(BB_SNAPSHOT)
+else
+$(ARCHIVE)/$(BUSYBOX_SOURCE):
+	$(WGET) https://busybox.net/downloads/$(BUSYBOX_SOURCE)
+
 $(D)/busybox: $(D)/bootstrap $(ARCHIVE)/$(BUSYBOX_SOURCE) $(PATCHES)/$(BUSYBOX_CONFIG)
 	$(START_BUILD)
 	$(REMOVE)/busybox$(BB_SNAPSHOT)
 	$(UNTAR)/$(BUSYBOX_SOURCE)
+endif
 	$(CHDIR)/busybox$(BB_SNAPSHOT); \
 		$(call apply_patches, $(BUSYBOX_PATCH)); \
 		install -m 0644 $(lastword $^) .config; \
