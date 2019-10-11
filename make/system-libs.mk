1e5b304789486e818d0f0af1d98a8de23d370bd2
Frankenstone <dampf_acc@yahoo.com>
2019-10-11 13:07:36 +0200

    update expat to 2.2.9

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1217,7 +1217,7 @@ $(D)/libiconv: $(D)/bootstrap $(ARCHIVE)/$(LIBICONV_SOURCE)
 #
 # expat
 #
-EXPAT_VER = 2.2.7
+EXPAT_VER = 2.2.9
 EXPAT_SOURCE = expat-$(EXPAT_VER).tar.bz2
 EXPAT_PATCH  = expat-$(EXPAT_VER)-libtool-tag.patch
 
