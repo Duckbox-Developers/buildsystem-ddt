e92313142421f5f1cd5b75391b09daa1aaa1ef64
BPanther <bpanther_ts@hotmail.com>
2019-11-17 17:11:03 +0100

    update openVPN 2.4.8

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1697,7 +1697,7 @@ $(D)/udpxy: $(D)/bootstrap $(ARCHIVE)/$(UDPXY_SOURCE)
 #
 # openvpn
 #
-OPENVPN_VER = 2.4.7
+OPENVPN_VER = 2.4.8
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VER).tar.xz
 
 $(ARCHIVE)/$(OPENVPN_SOURCE):
