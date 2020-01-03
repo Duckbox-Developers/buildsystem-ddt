f59e38b83abfee969ca895c731b8e89d61aa9262
BPanther <bpanther_ts@hotmail.com>
2020-01-03 15:32:53 +0100

    update pugixml 1.10

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1629,7 +1629,7 @@ $(D)/libroxml: $(D)/bootstrap $(ARCHIVE)/$(LIBROXML_SOURCE)
 #
 # pugixml
 #
-PUGIXML_VER = 1.9
+PUGIXML_VER = 1.10
 PUGIXML_SOURCE = pugixml-$(PUGIXML_VER).tar.gz
 PUGIXML_PATCH = pugixml-$(PUGIXML_VER)-config.patch
 
