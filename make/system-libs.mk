ae1f0b08359b98b87cdfff7b4126ee8a04718a82
BPanther <bpanther_ts@hotmail.com>
2020-01-08 17:49:26 +0100

    update libcurl 7.68.0

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -870,7 +870,7 @@ $(D)/ca-bundle: $(ARCHIVE)/$(CA-BUNDLE_SOURCE)
 ifeq ($(BOXARCH), sh4)
 LIBCURL_VER = 7.61.1
 else
-LIBCURL_VER = 7.67.0
+LIBCURL_VER = 7.68.0
 endif
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_PATCH = libcurl-$(LIBCURL_VER).patch
