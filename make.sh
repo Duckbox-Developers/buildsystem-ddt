c8d80ee68ebcdb48c500eef20a050015c1854fd2
BPanther <bpanther_ts@hotmail.com>
2020-01-12 04:21:16 +0100

    small change

--- make.sh
+++ make.sh
@@ -273,7 +273,7 @@ if [ $BOXARCH == 'arm' -o $BOXARCH == 'mips' ]; then
 	case $8 in
 		[1-3]) REPLY=$8;;
 		*)	echo -e "\nSelect GCC version:"
-			echo "   1)  GCC version 6.5.0"
+			echo "   1)  GCC version 6.5.0 (default)"
 			echo "   2)  GCC version 7.5.0"
 			echo "   3)  GCC version 8.3.0"
 			echo "   4)  GCC version 9.2.0"
