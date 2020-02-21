9c368a7a73c303fcb29383db41d8d1f948bd2dce
BPanther <bpanther_ts@hotmail.com>
2020-02-21 23:43:02 +0100

    update alsa-lib/utils 1.2.2 (sh4 alsa-lib still 1.2.1.2)

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1876,7 +1876,11 @@ $(D)/libusb_compat: $(D)/bootstrap $(D)/libusb $(ARCHIVE)/$(LIBUSB_COMPAT_SOURCE
 #
 # alsa-lib
 #
+ifeq ($(BOXARCH), sh4)
 ALSA_LIB_VER = 1.2.1.2
+else
+ALSA_LIB_VER = 1.2.2
+endif
 ALSA_LIB_SOURCE = alsa-lib-$(ALSA_LIB_VER).tar.bz2
 ALSA_LIB_PATCH  = alsa-lib-$(ALSA_LIB_VER).patch
 ALSA_LIB_PATCH += alsa-lib-$(ALSA_LIB_VER)-link_fix.patch
@@ -1919,7 +1923,7 @@ $(D)/alsa_lib: $(D)/bootstrap $(ARCHIVE)/$(ALSA_LIB_SOURCE)
 #
 # alsa-utils
 #
-ALSA_UTILS_VER = 1.2.1
+ALSA_UTILS_VER = 1.2.2
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VER).tar.bz2
 ALSA_UTILS_PATCH = alsa-utils-$(ALSA_UTILS_VER).patch
 
