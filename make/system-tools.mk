5656644d477b2644374b1e30ca10ae96e1754e48
BPanther <bpanther_ts@hotmail.com>
2020-02-21 23:39:59 +0100

    update util-linux 2.35.1

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -439,7 +439,7 @@ $(D)/e2fsprogs: $(D)/bootstrap $(D)/util_linux $(ARCHIVE)/$(E2FSPROGS_SOURCE)
 # util_linux
 #
 UTIL_LINUX_MAJOR = 2.35
-UTIL_LINUX_MINOR = -rc2
+UTIL_LINUX_MINOR = .1
 UTIL_LINUX_VER = $(UTIL_LINUX_MAJOR)$(UTIL_LINUX_MINOR)
 UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VER).tar.xz
 
