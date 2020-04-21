ba6001bf8a989127c7a99396131d31702e1507d1
BPanther <bpanther_ts@hotmail.com>
2020-04-21 18:06:41 +0200

    update optional openVPN 2.4.9

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1767,7 +1767,7 @@ $(D)/udpxy: $(D)/bootstrap $(ARCHIVE)/$(UDPXY_SOURCE)
 #
 # openvpn
 #
-OPENVPN_VER = 2.4.8
+OPENVPN_VER = 2.4.9
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VER).tar.xz
 
 $(ARCHIVE)/$(OPENVPN_SOURCE):
