7fd3c277f5d67b5633c80fd85244ca15625dfc79
Frankenstone <dampf_acc@yahoo.com>
2020-04-24 10:15:43 +0200

    prepare-for-bs: remove cfv, android tools is not needed

--- prepare-for-bs.sh
+++ prepare-for-bs.sh
@@ -99,7 +99,6 @@ PACKAGES="\
 	${UBUNTU:+pkg-config}           ${SUSE:+pkg-config}                                    ${GENTOO:+pkg-config}  \
 	${UBUNTU:+patch}                ${SUSE:+patch}                                         ${GENTOO:+patch}       \
 	${UBUNTU:+autopoint}                                                                                          \
-	${UBUNTU:+cfv}                                                                         ${GENTOO:+cfv}         \
 	${UBUNTU:+fakeroot}                                                                    ${GENTOO:+fakeroot}    \
 	${UBUNTU:+gawk}                                                                        ${GENTOO:+gawk}        \
 	${UBUNTU:+libglib2.0-bin}       ${SUSE:+glib2-devel}         ${FEDORA:+glib2-devel}    ${GENTOO:+glib:2}      \
@@ -116,7 +115,6 @@ PACKAGES="\
 	${UBUNTU:+libmount-dev}                                                                                       \
 	${UBUNTU:+mtools}                                                                                             \
 	${UBUNTU:+u-boot-tools}                                                                                       \
-	${UBUNTU:+android-tools-fsutils}                                                                              \
 	${UBUNTU:+curl}                                                                                               \
 ";
 
