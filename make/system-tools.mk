daddf2c2c47d4af8442d9a487f2ba99f7ef7e9e4
BPanther <bpanther_ts@hotmail.com>
2020-06-29 23:52:17 +0200

    update optional openssh 8.3p1

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1853,7 +1853,7 @@ $(D)/vpnc: $(D)/bootstrap $(D)/openssl $(D)/lzo $(D)/libgcrypt $(ARCHIVE)/$(VPNC
 #
 # openssh
 #
-OPENSSH_VER = 8.2p1
+OPENSSH_VER = 8.3p1
 OPENSSH_SOURCE = openssh-$(OPENSSH_VER).tar.gz
 
 $(ARCHIVE)/$(OPENSSH_SOURCE):
