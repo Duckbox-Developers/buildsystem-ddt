92e28ae09f5b147bc2ee5c5513400569b854daed
Frankenstone <dampf_acc@online.de>
2020-08-31 18:35:50 +0200

    Revert "host_parted 3.2 added - fix"
    This reverts commit 77a8cf96157b8078d67de7247c63969648267c46.

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -189,7 +189,6 @@ $(D)/host_parted: $(D)/directories $(ARCHIVE)/$(HOST_PARTED_SOURCE)
 			--prefix=$(HOST_DIR) \
 			--sbindir=$(HOST_DIR)/bin \
 			--disable-device-mapper \
-			--without-readline \
 		; \
 		$(MAKE) install
 	$(REMOVE)/parted-$(HOST_PARTED_VER)
