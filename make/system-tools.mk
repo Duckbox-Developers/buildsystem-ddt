d23333fc3c926d31ce689b1c2b62e88c141c8dd7
BPanther <bpanther_ts@hotmail.com>
2021-01-10 23:55:29 +0100

    fix busybox.git FreeBSD fix

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -15,6 +15,7 @@ BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-extra.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-extra2.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-flashcp-small-output.patch
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-block-telnet-internet.patch
+BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-changed_FreeBSD_fix.patch
 
 ifeq ($(BOXARCH), $(filter $(BOXARCH), arm mips))
 BUSYBOX_CONFIG = busybox-$(BUSYBOX_VER).config_arm
@@ -29,9 +30,6 @@ ifeq ($(BUSYBOX_SNAPSHOT), 1)
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-recursive_action-fix.patch
 ifeq ($(BOXARCH), $(filter $(BOXARCH), sh4 mips))
 BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-sh4-mips-revert_ifa_flags.patch
-endif
-ifeq ($(BOXARCH), $(filter $(BOXARCH), sh4))
-BUSYBOX_PATCH += busybox-$(BUSYBOX_VER)-sh4-revert_FreeBSD_fix.patch
 endif
 $(D)/busybox: $(D)/bootstrap $(PATCHES)/$(BUSYBOX_CONFIG)
 	$(START_BUILD)
