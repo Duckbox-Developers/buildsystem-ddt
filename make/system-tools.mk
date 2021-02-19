b260b539af7efe1e0ec314c77395f2a99429702a
BPanther <bpanther_ts@hotmail.com>
2021-02-19 23:55:00 +0100

    update optional sysstat 12.5.3

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -993,7 +993,7 @@ $(D)/fbshot: $(D)/bootstrap $(D)/libpng $(ARCHIVE)/$(FBSHOT_SOURCE)
 #
 # sysstat
 #
-SYSSTAT_VER = 12.5.2
+SYSSTAT_VER = 12.5.3
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VER).tar.bz2
 
 $(ARCHIVE)/$(SYSSTAT_SOURCE):
