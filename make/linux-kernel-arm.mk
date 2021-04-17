99473e86e5cfd6c44631e29f7b047720e40d913f
Frankenstone <dampf_acc@online.de>
2021-04-17 08:33:27 +0200

    Revert "linux-kernel-arm: add initramfs-subdirboot cpio, thx DboxOldie"
    This reverts commit 4e5d1b0e714f0c818485f03a9ba7a775c655c414.

--- make/linux-kernel-arm.mk
+++ make/linux-kernel-arm.mk
@@ -130,7 +130,6 @@ $(D)/kernel.do_prepare: $(ARCHIVE)/$(KERNEL_SRC) $(PATCHES)/armbox/$(KERNEL_CONF
 			$(PATCH)/$$i; \
 		done
 	install -m 644 $(PATCHES)/armbox/$(KERNEL_CONFIG) $(KERNEL_DIR)/.config
-	cp $(PATCHES)/armbox/initramfs-subdirboot.cpio.gz $(KERNEL_DIR)
 ifeq ($(OPTIMIZATIONS), $(filter $(OPTIMIZATIONS), kerneldebug debug))
 	@echo "Using kernel debug"
 	@grep -v "CONFIG_PRINTK" "$(KERNEL_DIR)/.config" > $(KERNEL_DIR)/.config.tmp
