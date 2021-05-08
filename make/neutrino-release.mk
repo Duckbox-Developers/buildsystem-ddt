29e5f504adc61c52a690f6dc8e60a46f3eae06cc
Frankenstone <dampf_acc@online.de>
2021-05-08 10:45:44 +0200

    neutrino-release: remove rc_913.jpg for ufs910, ufs922

--- make/neutrino-release.mk
+++ make/neutrino-release.mk
@@ -854,6 +854,7 @@ ifeq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
 	rm -f $(RELEASE_DIR)/sbin/mkfs.jfs
 	rm -f $(RELEASE_DIR)/sbin/jfs_tune
 	rm -f $(RELEASE_DIR)/etc/ssl/certs/ca-certificates.crt
+	rm -f $(RELEASE_DIR)/usr/share/tuxbox/neutrino/httpd/images/rc_913.jpg
 endif
 	rm -f $(RELEASE_DIR)/usr/lib/lua/5.2/*.la
 	rm -rf $(RELEASE_DIR)/lib/autofs
