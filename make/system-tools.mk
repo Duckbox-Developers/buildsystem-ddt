e27b12fadfd71b472fae8e0c4b4cb5ea75537cee
BPanther <bpanther_ts@hotmail.com>
2021-09-29 22:30:31 +0200

    update wget 1.21.2

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1260,7 +1260,7 @@ $(D)/avahi: $(D)/bootstrap $(D)/expat $(D)/libdaemon $(D)/dbus $(ARCHIVE)/$(AVAH
 #
 # wget
 #
-WGET_VER = 1.21.1
+WGET_VER = 1.21.2
 WGET_SOURCE = wget-$(WGET_VER).tar.gz
 WGET_PATCH = wget-$(WGET_VER).patch
 
