2b5c0f4f84702935f36491027d46c827f84e964a
BPanther <bpanther_ts@hotmail.com>
2021-09-29 22:27:24 +0200

    update libgd 2.3.3

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -905,7 +905,7 @@ $(D)/ca-bundle: $(ARCHIVE)/$(CA-BUNDLE_SOURCE)
 ifeq ($(BOXARCH), sh4)
 LIBCURL_VER = 7.61.1
 else
-LIBCURL_VER = 7.78.0
+LIBCURL_VER = 7.79.1
 endif
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_PATCH = libcurl-$(LIBCURL_VER).patch
