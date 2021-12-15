45bcfcff6170efa0d2db8dd668efddb07b61f59d
Frankenstone <dampf_acc@online.de>
2021-12-15 12:32:45 +0100

    neurino.mk: add cflag "-Wno-cast-function-type" for gcc 8.5.0, 9.4.0

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -60,6 +60,9 @@ N_CFLAGS      += -D__KERNEL_STRICT_NAMES
 N_CFLAGS      += -D__STDC_FORMAT_MACROS
 N_CFLAGS      += -D__STDC_CONSTANT_MACROS
 N_CFLAGS      += -fno-strict-aliasing -funsigned-char -ffunction-sections -fdata-sections
+ifeq ($(BS_GCC_VER), $(filter $(BS_GCC_VER), 8.5.0 9.4.0))
+N_CFLAGS      += -Wno-cast-function-type
+endif
 #N_CFLAGS      += -DCPU_FREQ
 N_CFLAGS      += $(LOCAL_NEUTRINO_CFLAGS)
 
