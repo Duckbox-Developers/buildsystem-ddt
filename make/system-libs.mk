b1efc378991405f97c1e34f131afd5ccbc66798e
BPanther <bpanther_ts@hotmail.com>
2022-03-13 15:49:12 +0100

    update libgcrypt 1.89

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -2508,7 +2508,7 @@ $(D)/libgpg-error: $(D)/bootstrap $(ARCHIVE)/$(LIBGPG_ERROR_SOURCE)
 #
 # libgcrypt
 #
-LIBGCRYPT_VER    = 1.8.8
+LIBGCRYPT_VER    = 1.8.9
 LIBGCRYPT_DIR    = libgcrypt-$(LIBGCRYPT_VER)
 LIBGCRYPT_SOURCE = libgcrypt-$(LIBGCRYPT_VER).tar.bz2
 LIBGCRYPT_URL    = https://gnupg.org/ftp/gcrypt/libgcrypt
