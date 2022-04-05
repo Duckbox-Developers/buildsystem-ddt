90eafc163214a1786533f57adfa8aebb6ea33189
BPanther <bpanther_ts@hotmail.com>
2022-04-05 16:06:09 +0200

    update zlib 1.2.12

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -478,12 +478,12 @@ $(D)/boost: $(D)/bootstrap $(ARCHIVE)/$(BOOST_SOURCE)
 #
 # zlib
 #
-ZLIB_VER = 1.2.11
+ZLIB_VER = 1.2.12
 ZLIB_SOURCE = zlib-$(ZLIB_VER).tar.xz
 ZLIB_Patch = zlib-$(ZLIB_VER).patch
 
 $(ARCHIVE)/$(ZLIB_SOURCE):
-	$(DOWNLOAD) https://sourceforge.net/projects/libpng/files/zlib/$(ZLIB_VER)/$(ZLIB_SOURCE)
+	$(DOWNLOAD) https://zlib.net/$(ZLIB_SOURCE)
 
 $(D)/zlib: $(D)/bootstrap $(ARCHIVE)/$(ZLIB_SOURCE)
 	$(START_BUILD)
