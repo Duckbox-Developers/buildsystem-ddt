26e7166f9a2ddcd537557d9e952b065412022b33
BPanther <bpanther_ts@hotmail.com>
2022-06-01 19:19:29 +0200

    update optional sysstat 12.6.0

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1036,7 +1036,7 @@ $(D)/fbshot: $(D)/bootstrap $(D)/libpng $(ARCHIVE)/$(FBSHOT_SOURCE)
 #
 # sysstat
 #
-SYSSTAT_VER = 12.5.6
+SYSSTAT_VER = 12.6.0
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VER).tar.bz2
 
 $(ARCHIVE)/$(SYSSTAT_SOURCE):
