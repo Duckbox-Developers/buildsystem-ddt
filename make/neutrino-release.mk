235225f8cc6580cf14d4d8731dc2812558da14b7
Frankenstone <dampf_acc@online.de>
2022-09-01 14:10:05 +0200

    Revert "neutrino-release: add missing folder /mnt/hosted"
    This reverts commit 782b0937f98c798375babd251ea1d22be4ef1c41.

--- make/neutrino-release.mk
+++ make/neutrino-release.mk
@@ -455,7 +455,7 @@ neutrino-release-base:
 	install -d $(RELEASE_DIR)/lib/{modules,udev,firmware,tuxbox}
 	install -d $(RELEASE_DIR)/media/{dvd,nfs,usb,sda1,sdb1}
 	ln -sf /hdd $(RELEASE_DIR)/media/hdd
-	install -d $(RELEASE_DIR)/mnt/{hdd,hosted,nfs,usb}
+	install -d $(RELEASE_DIR)/mnt/{hdd,nfs,usb}
 	install -d $(RELEASE_DIR)/mnt/mnt{0..7}
 	install -d $(RELEASE_DIR)/usr/{bin,lib,sbin,share}
 	install -d $(RELEASE_DIR)/usr/share/{fonts,tuxbox,udhcpc,zoneinfo,lua}
