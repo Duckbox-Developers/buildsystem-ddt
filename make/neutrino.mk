f6dd27466364d35507852742fdffd145e6d6f003
BPanther <bpanther_ts@hotmail.com>
2022-10-13 11:46:59 +0200

    E4HD 4K Ultra added (not yet ready)

--- make/neutrino.mk
+++ make/neutrino.mk
@@ -121,7 +121,7 @@ N_CONFIG_OPTS += --enable-flac
 NEUTRINO_DEPS += $(D)/flac
 endif
 
-ifeq ($(BOXTYPE), $(filter $(BOXTYPE), vuduo4k vuduo4kse vuuno4kse vuultimo4k vusolo4k dm8000))
+ifeq ($(BOXTYPE), $(filter $(BOXTYPE), vuduo4k vuduo4kse vuuno4kse vuultimo4k vusolo4k dm8000 e4hdultra))
 N_CONFIG_OPTS += --enable-graphlcd
 NEUTRINO_DEPS += $(D)/graphlcd
 endif
