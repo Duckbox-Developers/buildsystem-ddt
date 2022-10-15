5b8411b92c71714fc89b4df47b1d6cc8049a321c
BPanther <bpanther_ts@hotmail.com>
2022-10-15 23:35:08 +0200

    update optional ethtool 6.0

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1527,7 +1527,7 @@ $(D)/htop: $(D)/bootstrap $(D)/ncurses $(ARCHIVE)/$(HTOP_SOURCE)
 #
 # ethtool
 #
-ETHTOOL_VER = 5.19
+ETHTOOL_VER = 6.0
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VER).tar.xz
 ETHTOOL_PATCH = ethtool-$(ETHTOOL_VER).patch
 
