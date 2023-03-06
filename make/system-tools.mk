756ae1f2abd7641e22541c602aa8e000cb3607c1
BPanther <bpanther_ts@hotmail.com>
2023-03-07 00:22:29 +0100

    update midnight commander 4.8.29

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -729,7 +729,7 @@ $(D)/ntfs_3g: $(D)/bootstrap $(ARCHIVE)/$(NTFS_3G_SOURCE)
 #
 # mc
 #
-MC_VER = 4.8.28
+MC_VER = 4.8.29
 MC_SOURCE = mc-$(MC_VER).tar.xz
 MC_PATCH  = mc-$(MC_VER).patch
 ifeq ($(BOXARCH), sh4)
