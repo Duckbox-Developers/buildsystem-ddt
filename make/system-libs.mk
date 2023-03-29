73b671b618869dfca9f6016a2d437f9eb806943a
BPanther <bpanther_ts@hotmail.com>
2023-03-29 02:23:53 +0200

    arm/mips: update libcurl 8.0.1

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -876,7 +876,7 @@ $(D)/ca-bundle: $(ARCHIVE)/$(CA-BUNDLE_SOURCE)
 ifeq ($(BOXARCH), sh4)
 LIBCURL_VER = 7.61.1
 else
-LIBCURL_VER = 7.88.1
+LIBCURL_VER = 8.0.1
 endif
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_PATCH = libcurl-$(LIBCURL_VER).patch
