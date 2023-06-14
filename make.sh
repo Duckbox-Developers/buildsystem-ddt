e9182d5c18a83d5bb7071ce34d5c695bcb654d96
BPanther <bpanther_ts@hotmail.com>
2023-06-14 15:03:51 +0200

    update crosstool-NG 1.25.0.199_36ad0b1, GCC 11.3.0 -> GCC 11.4.0

--- make.sh
+++ make.sh
@@ -406,7 +406,7 @@ if [ $BOXARCH == 'arm' -o $BOXARCH == 'mips' ]; then
 			echo -e "   \033[01;32m3)  GCC version 8.5.0\033[00m"
 			echo "   4)  GCC version 9.5.0"
 			echo "   5)  GCC version 10.4.0"
-			echo "   6)  GCC version 11.3.0"
+			echo "   6)  GCC version 11.4.0"
 #			echo "   7)  GCC version 12.3.0 (not yet ready)"
 #			echo "   8)  GCC version 13.1.0 (not yet ready)"
 			read -p "Select GCC version (1-8)? ";;
@@ -418,7 +418,7 @@ if [ $BOXARCH == 'arm' -o $BOXARCH == 'mips' ]; then
 		3) BS_GCC_VER="8.5.0";;
 		4) BS_GCC_VER="9.5.0";;
 		5) BS_GCC_VER="10.4.0";;
-		6) BS_GCC_VER="11.3.0";;
+		6) BS_GCC_VER="11.4.0";;
 		7) BS_GCC_VER="12.3.0";;
 		8) BS_GCC_VER="13.1.0";;
 		*) BS_GCC_VER="8.5.0";;
