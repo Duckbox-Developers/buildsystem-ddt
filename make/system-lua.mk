181423e641f53f442893f26ef0bf4e5738462c10
BPanther <bpanther_ts@hotmail.com>
2023-07-30 14:27:36 +0200

    fix download luasoap

--- make/system-lua.mk
+++ make/system-lua.mk
@@ -129,12 +129,13 @@ $(D)/luafeedparser: $(D)/bootstrap $(D)/lua $(D)/luasocket $(D)/luaexpat $(ARCHI
 #
 # luasoap
 #
-LUASOAP_VER = 3.0
+LUASOAP_VER = 3_0_1
 LUASOAP_SOURCE = luasoap-$(LUASOAP_VER).tar.gz
-LUASOAP_PATCH = luasoap-$(LUASOAP_VER).patch
+#LUASOAP_PATCH = luasoap-$(LUASOAP_VER).patch
 
 $(ARCHIVE)/$(LUASOAP_SOURCE):
-	$(DOWNLOAD) https://github.com/downloads/tomasguisasola/luasoap/$(LUASOAP_SOURCE)
+#	$(DOWNLOAD) https://github.com/downloads/tomasguisasola/luasoap/$(LUASOAP_SOURCE)
+	$(DOWNLOAD) https://github.com/tomasguisasola/luasoap/archive/refs/tags/v$(LUASOAP_VER).tar.gz -O $(ARCHIVE)/$(LUASOAP_SOURCE)
 
 $(D)/luasoap: $(D)/bootstrap $(D)/lua $(D)/luasocket $(D)/luaexpat $(ARCHIVE)/$(LUASOAP_SOURCE)
 	$(START_BUILD)
