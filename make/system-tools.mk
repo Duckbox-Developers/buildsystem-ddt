683fa40507e9f4447031732b8f188f8064ec4bfe
BPanther <bpanther_ts@hotmail.com>
2023-08-17 07:29:29 +0200

    update midnight commander 4.8.30

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -734,13 +734,12 @@ $(D)/ntfs_3g: $(D)/bootstrap $(ARCHIVE)/$(NTFS_3G_SOURCE)
 #
 # mc
 #
-MC_VER = 4.8.29
+MC_VER = 4.8.30
 MC_SOURCE = mc-$(MC_VER).tar.xz
 MC_PATCH  = mc-$(MC_VER).patch
 ifeq ($(BOXARCH), sh4)
 MC_PATCH += mc-$(MC_VER)_sh4.patch
 endif
-MC_PATCH += mc-$(MC_VER)-fix_ftp_listing.patch
 
 $(ARCHIVE)/$(MC_SOURCE):
 	$(DOWNLOAD) ftp.midnight-commander.org/$(MC_SOURCE)
