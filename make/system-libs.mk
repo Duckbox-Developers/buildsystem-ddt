ea16658ec49e920a5f820bf9c2c8af6f44365f35
BPanther <bpanther_ts@hotmail.com>
2023-09-03 15:16:55 +0200

    update zlib 1.3

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -448,7 +448,7 @@ $(D)/boost: $(D)/bootstrap $(ARCHIVE)/$(BOOST_SOURCE)
 #
 # zlib
 #
-ZLIB_VER = 1.2.13
+ZLIB_VER = 1.3
 ZLIB_SOURCE = zlib-$(ZLIB_VER).tar.xz
 ZLIB_Patch = zlib-$(ZLIB_VER).patch
 
