30973ed160a48a26bc38cb187b5194a0d157e22b
BPanther <bpanther_ts@hotmail.com>
2023-11-06 21:07:07 +0100

    update pugixml GIT 6909df2

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1645,7 +1645,7 @@ $(D)/libroxml: $(D)/bootstrap $(ARCHIVE)/$(LIBROXML_SOURCE)
 #
 # pugixml
 #
-PUGIXML_VER = db78afc
+PUGIXML_VER = 6909df2
 PUGIXML_PATCH = pugixml-config.patch
 
 $(D)/pugixml: $(D)/bootstrap $(ARCHIVE)/$(PUGIXML_SOURCE)
