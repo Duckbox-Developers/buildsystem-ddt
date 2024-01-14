9554afbf000b7951f267aaea9df45555a6dd8afb
BPanther <bpanther_ts@hotmail.com>
2024-01-14 13:17:09 +0100

    fix mdev-modload.sh (thx DboxOldie)

--- root/etc/mdev/mdev-modload.sh
+++ root/etc/mdev/mdev-modload.sh
@@ -1,33 +1,37 @@
 #!/bin/sh
-while [ -z "$(mount | grep '/dev ')" ];do sleep 1;done
+while [ -z "$(mount | grep '/dev ')" ]; do sleep 1; done
 MODALIAS=$1
 ENABLE_LOG=1
 LOG="/dev/.mdev/modalias.log"
+
 #
 loginfo()
 {
-OUT=$1
-logleft="[$ACTION] $(date +'%H:%M:%S') [$MDEV]"
-if [ "$ENABLE_LOG" == "1" ];then
-	echo "$logleft $OUT" >> $LOG
-else
-	echo "$logleft $OUT"
-fi
-}
-# load modul
-load_modul() {
-modul=""
-[ "$1" == "v067B" ] && modul="pl2303"
-[ "$1" == "v0403" ] && modul="ftdi_sio"
-if [ -n "$modul" ];then
-	if [ ! -e /sys/module/$modul ];then
-		insmod $modul
-		loginfo "loading "$modul".ko"
+	OUT=$1
+	logleft="[$ACTION] $(date +'%H:%M:%S') [$MDEV]"
+	if [ "$ENABLE_LOG" == "1" ];then
+		echo "$logleft $OUT" >> $LOG
 	else
-		loginfo $modul".ko already loaded"
+		echo "$logleft $OUT"
 	fi
-fi
 }
+
+# load module
+load_modul()
+{
+	modul=""
+	[ "$1" == "v067B" ] && modul="pl2303.ko"
+	[ "$1" == "v0403" ] && modul="ftdi_sio.ko"
+	if [ -n "$modul" ];then
+		if [ ! -e /sys/module/$modul ];then
+			insmod /lib/modules/$modul
+			loginfo "loading $modul"
+		else
+			loginfo "$modul already loaded"
+		fi
+	fi
+}
+
 #
 if [ -n "$MODALIAS" ];then
 	[ ! -d /dev/.mdev ] && mkdir -p /dev/.mdev
