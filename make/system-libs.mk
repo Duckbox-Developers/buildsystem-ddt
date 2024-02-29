869248da87e43fb97a4d66d5d0d04e601ac87b11
limes007 <39467727+limes007@users.noreply.github.com>
2024-02-29 15:59:01 +0100

    adapt source and version of libpopt (not longer available)

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1634,11 +1634,11 @@ endif
 #
 # libpopt
 #
-LIBPOPT_VER = 1.16
+LIBPOPT_VER = 1.19
 LIBPOPT_SOURCE = popt-$(LIBPOPT_VER).tar.gz
 
 $(ARCHIVE)/$(LIBPOPT_SOURCE):
-	$(DOWNLOAD) ftp://anduin.linuxfromscratch.org/BLFS/popt/$(LIBPOPT_SOURCE)
+	$(DOWNLOAD) ftp://ftp.rpm.org/pub/rpm/popt/releases/popt-1.x/$(LIBPOPT_SOURCE)
 
 $(D)/libpopt: $(D)/bootstrap $(ARCHIVE)/$(LIBPOPT_SOURCE)
 	$(START_BUILD)
