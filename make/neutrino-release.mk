ea88f5a4a9d6a77d54745593616daca8d9bf7588
BPanther <bpanther_ts@hotmail.com>
2024-03-21 21:38:15 +0100

    unused libs libBrokenLocale and libpcprofile removed

--- make/neutrino-release.mk
+++ make/neutrino-release.mk
@@ -935,6 +935,8 @@ endif
 	rm -f $(RELEASE_DIR)/lib/libthread_db*
 	rm -f $(RELEASE_DIR)/lib/libanl*
 	rm -f $(RELEASE_DIR)/lib/libmemusage.so
+	rm -f $(RELEASE_DIR)/lib/libBrokenLocale*
+	rm -f $(RELEASE_DIR)/lib/libpcprofile.so
 	rm -rf $(RELEASE_DIR)/lib/modules/$(KERNEL_VER)
 	rm -rf $(RELEASE_DIR)/lib/modules/$(KERNEL_VER)-$(BOXTYPE)
 	rm -rf $(RELEASE_DIR)/usr/lib/alsa
