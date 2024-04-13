d6fd7b409dee98f208d181ef7c15709fa45df681
BPanther <bpanther_ts@hotmail.com>
2024-04-13 21:17:57 +0200

    ufs910/22: hd-idle removed

--- make/bootstrap.mk
+++ make/bootstrap.mk
@@ -318,9 +318,9 @@ SYSTEM_TOOLS += $(D)/zlib
 SYSTEM_TOOLS += $(D)/sysvinit
 SYSTEM_TOOLS += $(D)/diverse-tools
 SYSTEM_TOOLS += $(D)/e2fsprogs
-SYSTEM_TOOLS += $(D)/hdidle
 SYSTEM_TOOLS += $(D)/portmap
 ifneq ($(BOXTYPE), $(filter $(BOXTYPE), ufs910 ufs922))
+SYSTEM_TOOLS += $(D)/hdidle
 SYSTEM_TOOLS += $(D)/jfsutils
 SYSTEM_TOOLS += $(D)/nfs_utils
 endif
