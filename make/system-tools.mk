b8379abb77eb3ce9526350b5987d0f083de07575
BPanther <bpanther_ts@hotmail.com>
2024-05-08 02:42:22 +0200

    unused coreutils removed

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -1096,32 +1096,6 @@ $(D)/sed: $(D)/bootstrap $(ARCHIVE)/$(SED_SOURCE)
 	$(TOUCH)
 
 #
-# coreutils
-#
-COREUTILS_VER = 8.23
-COREUTILS_SOURCE = coreutils-$(COREUTILS_VER).tar.xz
-COREUTILS_PATCH = coreutils-$(COREUTILS_VER).patch
-
-$(ARCHIVE)/$(COREUTILS_SOURCE):
-	$(DOWNLOAD) https://ftp.gnu.org/gnu/coreutils/$(COREUTILS_SOURCE)
-
-$(D)/coreutils: $(D)/bootstrap $(D)/openssl $(ARCHIVE)/$(COREUTILS_SOURCE)
-	$(START_BUILD)
-	$(REMOVE)/coreutils-$(COREUTILS_VER)
-	$(UNTAR)/$(COREUTILS_SOURCE)
-	$(CHDIR)/coreutils-$(COREUTILS_VER); \
-		$(call apply_patches, $(COREUTILS_PATCH)); \
-		export fu_cv_sys_stat_statfs2_bsize=yes; \
-		$(CONFIGURE) \
-			--prefix=/usr \
-			--enable-largefile \
-		; \
-		$(MAKE); \
-		$(MAKE) install DESTDIR=$(TARGET_DIR)
-	$(REMOVE)/coreutils-$(COREUTILS_VER)
-	$(TOUCH)
-
-#
 # smartmontools
 #
 SMARTMONTOOLS_VER = 7.4
