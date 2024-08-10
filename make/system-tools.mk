fc336dbd3a1daa9da26437892a60f58e1bdbd07f
BPanther <bpanther_ts@hotmail.com>
2024-08-10 02:20:12 +0200

    update sysvinit 3.10

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -158,7 +158,7 @@ $(D)/module_init_tools: $(D)/bootstrap $(D)/lsb $(ARCHIVE)/$(MODULE_INIT_TOOLS_S
 #
 # sysvinit
 #
-SYSVINIT_VER = 3.09
+SYSVINIT_VER = 3.10
 SYSVINIT_SOURCE = sysvinit-$(SYSVINIT_VER).tar.xz
 SYSVINIT_PATCH  = sysvinit-$(SYSVINIT_VER)-crypt-lib.patch
 SYSVINIT_PATCH += sysvinit-$(SYSVINIT_VER)-change-INIT_FIFO.patch
