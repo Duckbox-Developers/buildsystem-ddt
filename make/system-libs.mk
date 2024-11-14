1dbdd9c03e21c5bb6bf8ca446e9af00bd5fdc4fe
BPanther <bpanther_ts@hotmail.com>
2024-11-14 18:14:34 +0100

    sh4: update alsa-lib/utils 1.2.13

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1771,7 +1771,7 @@ $(D)/libusb_compat: $(D)/bootstrap $(D)/libusb $(ARCHIVE)/$(LIBUSB_COMPAT_SOURCE
 #
 # alsa-lib
 #
-ALSA_LIB_VER = 1.2.12
+ALSA_LIB_VER = 1.2.13
 ALSA_LIB_SOURCE = alsa-lib-$(ALSA_LIB_VER).tar.bz2
 ALSA_LIB_PATCH  = alsa-lib-$(ALSA_LIB_VER).patch
 ALSA_LIB_PATCH += alsa-lib-$(ALSA_LIB_VER)-link_fix.patch
@@ -1818,7 +1818,7 @@ $(D)/alsa_lib: $(D)/bootstrap $(ARCHIVE)/$(ALSA_LIB_SOURCE)
 #
 # alsa-utils
 #
-ALSA_UTILS_VER = 1.2.12
+ALSA_UTILS_VER = 1.2.13
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VER).tar.bz2
 ALSA_UTILS_PATCH = alsa-utils-$(ALSA_UTILS_VER).patch
 
