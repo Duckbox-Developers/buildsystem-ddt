f194f24511e95c695b087b9665d38f696b4693d9
BPanther <bpanther_ts@hotmail.com>
2024-11-14 17:42:30 +0100

    update expat 2.6.4

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -1136,7 +1136,7 @@ $(D)/libiconv: $(D)/bootstrap $(ARCHIVE)/$(LIBICONV_SOURCE)
 #
 # expat
 #
-EXPAT_VER = 2.6.3
+EXPAT_VER = 2.6.4
 EXPAT_SOURCE = expat-$(EXPAT_VER).tar.bz2
 EXPAT_PATCH  = expat-$(EXPAT_VER)-libtool-tag.patch
 
