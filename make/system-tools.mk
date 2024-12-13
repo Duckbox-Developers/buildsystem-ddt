50ed857e3637b7428f7915796daa25a473962b24
BPanther <bpanther_ts@hotmail.com>
2024-12-13 01:51:11 +0100

    update optional socat 1.8.0.2

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -700,7 +700,7 @@ $(D)/mc: $(D)/bootstrap $(D)/ncurses $(D)/libglib2 $(ARCHIVE)/$(MC_SOURCE)
 #
 # socat
 #
-SOCAT_VER = 1.8.0.1
+SOCAT_VER = 1.8.0.2
 SOCAT_SOURCE = socat-$(SOCAT_VER).tar.gz
 SOCAT_PATCH = socat-$(SOCAT_VER).patch
 
