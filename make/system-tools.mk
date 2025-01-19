f425ea517f5a56e699b1b4ac2962b79a1cb69ff7
BPanther <bpanther_ts@hotmail.com>
2025-01-19 13:54:10 +0100

    update e2fsprogs 1.47.2

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -305,7 +305,7 @@ $(D)/portmap: $(D)/bootstrap $(D)/lsb $(ARCHIVE)/$(PORTMAP_SOURCE) $(ARCHIVE)/po
 #
 # e2fsprogs
 #
-E2FSPROGS_VER = 1.47.1
+E2FSPROGS_VER = 1.47.2
 E2FSPROGS_SOURCE = e2fsprogs-$(E2FSPROGS_VER).tar.gz
 E2FSPROGS_PATCH = e2fsprogs-$(E2FSPROGS_VER).patch
 
@@ -316,7 +316,7 @@ E2FSPROGS_ARGS = --disable-resizer
 endif
 
 $(ARCHIVE)/$(E2FSPROGS_SOURCE):
-	$(DOWNLOAD) https://sourceforge.net/projects/e2fsprogs/files/e2fsprogs/v$(E2FSPROGS_VER)/$(E2FSPROGS_SOURCE)
+	$(DOWNLOAD) https://kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v$(E2FSPROGS_VER)/$(E2FSPROGS_SOURCE)
 
 $(D)/e2fsprogs: $(D)/bootstrap $(D)/util_linux $(ARCHIVE)/$(E2FSPROGS_SOURCE)
 	$(START_BUILD)
