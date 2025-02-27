05b67ecd4a153320a992eefd7f8c0660172bd501
BPanther <bpanther_ts@hotmail.com>
2025-02-27 10:44:19 +0100

    update libpng 1.6.47

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -681,7 +681,7 @@ $(D)/libjpeg_turbo: $(D)/bootstrap $(ARCHIVE)/$(LIBJPEG_TURBO_SOURCE)
 #
 # libpng
 #
-LIBPNG_VER = 1.6.45
+LIBPNG_VER = 1.6.47
 LIBPNG_VER_X = 16
 LIBPNG_SOURCE = libpng-$(LIBPNG_VER).tar.xz
 
