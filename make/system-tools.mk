ceb8e7e63c98191af255e25fc5737ce1eba1e7db
BPanther <bpanther_ts@hotmail.com>
2025-07-04 12:30:32 +0200

    update util_linux 2.41.1

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -382,13 +382,8 @@ $(D)/e2fsprogs: $(D)/bootstrap $(D)/util_linux $(ARCHIVE)/$(E2FSPROGS_SOURCE)
 #
 # util_linux
 #
-#ifeq ($(BOXARCH), sh4)
-#UTIL_LINUX_MAJOR = 2.40
-#UTIL_LINUX_MINOR = .4
-#else
 UTIL_LINUX_MAJOR = 2.41
-#UTIL_LINUX_MINOR = 
-#endif
+UTIL_LINUX_MINOR = .1
 UTIL_LINUX_VER = $(UTIL_LINUX_MAJOR)$(UTIL_LINUX_MINOR)
 UTIL_LINUX_SOURCE = util-linux-$(UTIL_LINUX_VER).tar.xz
 
