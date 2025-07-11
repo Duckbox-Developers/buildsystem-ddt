ae65d3e0a2413f48eaddad730f7a7fb06d89d0ae
BPanther <bpanther_ts@hotmail.com>
2025-07-11 14:16:18 +0200

    update e2fsprogs 1.47.3

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -314,7 +314,7 @@ $(D)/portmap: $(D)/bootstrap $(D)/lsb $(ARCHIVE)/$(PORTMAP_SOURCE) $(ARCHIVE)/po
 #
 # e2fsprogs
 #
-E2FSPROGS_VER = 1.47.2
+E2FSPROGS_VER = 1.47.3
 E2FSPROGS_SOURCE = e2fsprogs-$(E2FSPROGS_VER).tar.gz
 E2FSPROGS_PATCH = e2fsprogs-$(E2FSPROGS_VER).patch
 
