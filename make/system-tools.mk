90837b55a4e055c4ee20cc13ae00267a0def4013
BPanther <bpanther_ts@hotmail.com>
2025-08-21 14:09:47 +0200

    update optional sysstat 12.7.8, fix download url

--- make/system-tools.mk
+++ make/system-tools.mk
@@ -914,11 +914,12 @@ $(D)/fbshot: $(D)/bootstrap $(D)/libpng $(ARCHIVE)/$(FBSHOT_SOURCE)
 #
 # sysstat
 #
-SYSSTAT_VER = 12.6.2
+SYSSTAT_VER = 12.7.8
 SYSSTAT_SOURCE = sysstat-$(SYSSTAT_VER).tar.xz
 
 $(ARCHIVE)/$(SYSSTAT_SOURCE):
-	$(DOWNLOAD) http://pagesperso-orange.fr/sebastien.godard/$(SYSSTAT_SOURCE)
+#	$(DOWNLOAD) http://pagesperso-orange.fr/sebastien.godard/$(SYSSTAT_SOURCE)
+	$(DOWNLOAD) https://github.com/sysstat/sysstat/archive/refs/tags/v$(SYSSTAT_VER).tar.gz -O $(ARCHIVE)/$(SYSSTAT_SOURCE)
 
 $(D)/sysstat: $(D)/bootstrap $(ARCHIVE)/$(SYSSTAT_SOURCE)
 	$(START_BUILD)
