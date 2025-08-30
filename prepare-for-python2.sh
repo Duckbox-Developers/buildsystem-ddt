8ba8a347b1528d83cca7c0bb794302a295be08ea
BPanther <bpanther_ts@hotmail.com>
2025-08-30 20:15:58 +0200

    prepare-for-python2.sh added for newer ubuntu versions without python2

---
+++ prepare-for-python2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(id -u)" = "0" ]; then
+	echo "You are running as root. Do not do this."
+	exit 1
+fi
+
+ARCHIVE=$HOME/Archive
+mkdir -p $ARCHIVE
+
+if [ ! -e $ARCHIVE/Python-2.7.18.tar.xz ]; then
+	wget https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tar.xz -O $ARCHIVE/Python-2.7.18.tar.xz
+fi
+
+echo "Build of Python 2.7.18..."
+
+[ -e $HOME/Python-2.7.18 ] && rm -rf $HOME/Python-2.7.18
+cd $HOME
+tar xJf $ARCHIVE/Python-2.7.18.tar.xz
+cd $HOME/Python-2.7.18
+./configure --enable-optimizations
+make && sudo make install && cd $HOME && rm -rf $HOME/Python-2.7.18 && echo "Build and Install of Python 2.7.18 - Done."
