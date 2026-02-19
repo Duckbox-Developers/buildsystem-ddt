74c54d92027aae6a94813e57f3d922a5b56ef053
BPanther <bpanther_ts@hotmail.com>
2026-02-19 11:42:46 +0100

    update optional libgcrypt 1.12.0

--- make/system-libs.mk
+++ make/system-libs.mk
@@ -2254,7 +2254,7 @@ $(D)/libgpg-error: $(D)/bootstrap $(ARCHIVE)/$(LIBGPG_ERROR_SOURCE)
 #
 # libgcrypt
 #
-LIBGCRYPT_VER    = 1.11.2
+LIBGCRYPT_VER    = 1.12.0
 LIBGCRYPT_DIR    = libgcrypt-$(LIBGCRYPT_VER)
 LIBGCRYPT_SOURCE = libgcrypt-$(LIBGCRYPT_VER).tar.bz2
 LIBGCRYPT_URL    = https://gnupg.org/ftp/gcrypt/libgcrypt
